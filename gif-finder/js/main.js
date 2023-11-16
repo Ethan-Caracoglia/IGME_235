9e8225a36ff009c809c3f3c1688e2431a63d1cef
Ethan-Caracoglia <ethancaracoglia@gmail.com>
2023-11-15 22:05:35 -0500

    Repaired

--- gif-finder/js/main.js
+++ gif-finder/js/main.js
@@ -1,92 +1,117 @@
+
 "use strict";
-    
-    // 1
-  	window.onload = (e) => {document.querySelector("#search").onclick = searchButtonClicked};
-	
-	// 2
-	let displayTerm = "";
-	
-	// 3
-	function searchButtonClicked(){
-		console.log("searchButtonClicked() called");
+// 1
+  window.onload = (e) => {document.querySelector("#search").onclick = searchButtonClicked};
 
-        // 1 - main entry point to web service
-	    const G_URL = "https://api.giphy.com/v1/gifs/search?";
-	
-	    // Public API key from here: https://developers.giphy.com/docs/
-	    const G_KEY = "5PuWjWVnwpHUQPZK866vd7wQ2qeCeqg7";
+// 2
+let displayTerm = "";
 
-        let url = G_URL;
-        url += "api_key=" + G_KEY;
+// 3
+function searchButtonClicked(){
+    console.log("searchButtonClicked() called");
 
-        let term = document.querySelector("#searchterm").value;
-        displayTerm = term;
+    //URL
+    const GIPHY_URL = "https://api.giphy.com/v1/gifs/search?";
 
-        term = term.trim();
+    //Public API Key
+    let GIPHY_KEY = "5PuWjWVnwpHUQPZK866vd7wQ2qeCeqg7";
 
-        term = encodeURIComponent(term);
+    //Construct URL on a string
+    let url = GIPHY_URL;
+    url += "api_key=" + GIPHY_KEY;
 
-        if(term.length < 1) return;
+    //Parse the user entered data
+    let term = document.querySelector("#searchterm").value;
+    displayTerm = term;
 
-        url += "&q=" + term;
+    //get rid of spaces
+    term = term.trim();
 
-        let limit = document.querySelector("#limit").value;
-        url += "&limit=" + limit;
-    
-        document.querySelector("#status").innerHTML = "<b>Searching for '" + displayTerm + "'</b>";
+    //encode spaces and special characters
+    term = encodeURIComponent(term);
 
-        console.log(url);
-    }
+    //If there is no term entered then return
+    if (term.length < 1) {return;};
 
-    function getData(url) {
-        let xhr = new XMLHttpRequest();
+    //append the search term to the URL
+    url += "&q=" + term;
 
-        xhr.onload = dataLoaded;
+    //grab the chosen limit
+    let limit = document.querySelector("#limit").value;
+    url += "&limit=" + limit;
 
-        xhr.onerror = dataError;
+    //Update the UI
+    document.querySelector("#status").innerHTML = "<b>Searching for '" + displayTerm + "'</b>";
 
-        xhr.open("GET", url);
-        xhr.send();
-    }
+    //See what the url looks like.
+    console.log(url);
 
-    function dataLoaded(e) {
-        let xhr = e.target;
+    getData(url);
+}
 
-        console.log(xhr.responseText);
+function getData(url) {
+    //create xhr objhect
+    let xhr = new XMLHttpRequest();
 
-        let obj = JSON.parse(xhr.responseText);
+    //set the onload event handler
+    xhr.onload = dataLoaded;
 
+    //Set the onerror handler
+    xhr.onerror = dataError;
+
+    //open the connection and send the request
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function dataLoaded(e) {
+    //event xhr is set to the event target
+    let xhr = e.target;
+
+    //make the response text the JSON file we just downloaded
+    console.log(xhr.responseText);
+
+    let obj = JSON.parse(xhr.responseText);
+
+    //if there are not results - return
     if (!obj.data || obj.data.length == 0) {
         document.querySelector("#status").innerHTML = "<b>No results found for '" + displayTerm + "'</b>";
-        return;
+        return; // ball out
     }
 
+    //Start building an HTML string we will display to the user
     let results = obj.data;
     console.log("results.length = " + results.length);
     let bigString = "<p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p>";
 
+    //loop through the array of results
     for (let i = 0; i < results.length; i++) {
         let result = results[i];
 
-        let smallURL = result.image.fixed_width_small.url;
-        if(!smallURL) smallURL = "images/no-image-found.png";
+        //Get the gif URL
+        let smallURL = result.images.fixed_width_small.url;
+        if (!smallURL) {
+            smallURL = "images/no-image-found.png";
+        }
 
+        //GEt the URL to the page
         let url = result.url;
 
-        let line = `<div class='result'><img src='${smallURL}' title='${result.id}' />`;
-        line += `<span><a target='_blank' href='${url}'>View on Giphy</a><br>'${result.rating.toUpperCase()}'</span></div>`;
+        //build a div for each result
+        let line = `<div class ='result'><img src='${smallURL}' title='${result.id}'>`;
+        line += `<span><a target='_blank' href='${url}'>View on Giphy</a></span></div>`;
 
         bigString += line;
     }
 
+    //set the content equal to the bigString
     document.querySelector("#content").innerHTML = bigString;
 
+    //Update the status
     document.querySelector("#status").innerHTML = "<b>Success!</b>";
+}
 
-    }
-
-    function dataError(e) {
-        console.log("An error has ocurred");
-    }
-
-    
+function dataError(e) {
+    //called during error occurrence
+    console.log("An error occurred");
+}
